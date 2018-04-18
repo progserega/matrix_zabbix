1c52b892ffed2ce4c5681f988ae35b0e620a9486
Семёнов Сергей <semenov@rsprim.ru>
2018-04-18 16:32:19 +1000

    отсылка

--- matrix_send_message.py
+++ matrix_send_message.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 import config as conf
 from matrix_client.client import MatrixClient
+import sys
+
+zbx_to = sys.argv[1]
+zbx_subject = sys.argv[2]
+zbx_body = sys.argv[3]
+
 
 client = MatrixClient(conf.server)
 
@@ -14,5 +20,10 @@ token = client.login_with_password(username=conf.username, password=conf.passwor
 room_info = client.join_room(conf.room_info)
 room_major = client.join_room(conf.room_major)
 room_critical = client.join_room(conf.room_critical)
-ret=room_info.send_text("Hello!")
+
+text="""%(zbx_subject)s
+%(zbx_body)s
+"""%("zbx_subject":zbx_subject, "zbx_body":zbx_body)
+
+ret=room_info.send_text(text)
 print("ret=",ret)
